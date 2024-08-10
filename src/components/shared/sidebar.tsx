3d54f10ecb28a07c823cfdb00e32d66f5c0ff2bf
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-08-10 23:47:13 +0600

    feat: user profile button updated

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import { MoveRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="fixed z-30 hidden h-screen w-full shrink-0 lg:sticky lg:block bg-secondary/50 border-r border-r-border">
+    <div className="fixed z-30 hidden h-screen w-full shrink-0 lg:sticky bg-secondary/50 border-r border-r-border lg:flex flex-col justify-between">
       {/* lgoo */}
       <Link
         href="/dashboard/overview"
@@ -22,7 +23,13 @@ const Sidebar = () => {
       </Link>
 
       {/* user button */}
-      <div className="px-6 py-4 w-full bg-secondary mt-4">
+      <div className="w-full px-6 pb-4 flex items-center justify-between">
+        <span className="flex items-center gap-2">
+          <p className="text-sm text-muted-foreground">
+            Click to view your user account
+          </p>
+          <MoveRight size={14} className="text-sm text-muted-foreground" />
+        </span>
         <UserButton />
       </div>
     </div>
