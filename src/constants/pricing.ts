3abfbc5d7984f27da0d48d08aabe111ccf32c114
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-08-18 22:08:47 +0600

    feat: add pricing constant data

---
+++ src/constants/pricing.ts
@@ -0,0 +1,32 @@
+export const pricing = [
+  {
+    id: "1",
+    name: "Hobby",
+    price: "FREE",
+    description: "Get first your personal website",
+    features: [
+      "Claime your username",
+      "Generate your profile",
+      "4 linked social accounts",
+      "2 experience cards",
+      "1 project card with thumbnails",
+      "Community support",
+    ],
+  },
+  {
+    id: "2",
+    name: "Business",
+    price: "CUSTOM",
+    description: "Let us know your requirements",
+    features: [
+      "All features from hobby plans",
+      "Your custom requirements",
+      //   "Claime your username",
+      //   "Generate your profile",
+      //   "8 linked social accounts",
+      //   "4 experience cards",
+      //   "3 project card with thumbnails",
+      //   "Meet support (30mins) for exeptional cases",
+    ],
+  },
+];
