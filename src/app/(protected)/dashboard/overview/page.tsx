e51578963549c1a573dc7c76b15693680fced8a8
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-08-23 20:12:48 +0600

    feat: update overview page metadata

--- src/app/(protected)/dashboard/overview/page.tsx
+++ src/app/(protected)/dashboard/overview/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Overview from "@/components/pages/overview/overview";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Rulink - Overview",
+  description: "Manage your personal informations",
   openGraph: {
     title: "Rulink - Overview",
+    description: "Manage your personal informations",
+    type: "website",
+    url: "https://rulink.vercel.app",
+    siteName: "Rulink",
+    images: ["https://i.ibb.co/nmPJVPm/food-delivary-website.png"],
   },
 };
 
