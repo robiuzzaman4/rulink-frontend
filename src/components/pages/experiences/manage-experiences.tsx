22c0eef406e6dcefe2dd3b9820baefe68b7d55ba
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-08-26 00:14:59 +0600

    feat: initilize your experiences component

--- src/components/pages/experiences/manage-experiences.tsx
+++ src/components/pages/experiences/manage-experiences.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import IconButton from "@/components/shared/icon-button";
-import { FolderClosed } from "lucide-react";
 import React from "react";
-import YourProjects from "@/components/pages/projects/your-projects";
-import AddNewProjectForm from "@/components/pages/projects/add-new-project-form";
+import IconButton from "@/components/shared/icon-button";
+import { BriefcaseBusiness } from "lucide-react";
+import YourExperiences from "@/components/pages/experiences/your-experiences";
 
 const ManageExperiences = () => {
   return (
@@ -12,14 +11,12 @@ const ManageExperiences = () => {
       {/* header */}
       <div className="grid gap-2 px-4 sm:px-6 py-4">
         <IconButton>
-          <FolderClosed size={20} />
+          <BriefcaseBusiness size={20} />
         </IconButton>
         <h5 className="text-lg font-medium font-satoshi">Manage Experiences</h5>
       </div>
-      {/* your projects */}
-      <YourProjects />
-      {/* add new project */}
-      <AddNewProjectForm />
+      {/* your experiences */}
+      <YourExperiences />
     </div>
   );
 };
---
+++ src/components/pages/experiences/your-experiences.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import useUserByEmail from "@/hooks/useUserByEmail";
+import { Loader } from "lucide-react";
+
+const YourExperiences = () => {
+  // === get user info from db ===
+  const { isLoading } = useUserByEmail();
+
+  return (
+    <>
+      <div className="w-full px-1 pb-1">
+        <div className="w-full bg-background p-4 sm:p-6 rounded-xl border border-border shadow-sm flex flex-col gap-4">
+          <h5 className="text-lg font-medium font-satoshi">Your Experiences</h5>
+          {isLoading && (
+            <>
+              <Loader size={16} className="animate-spin w-fit mx-auto" />
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default YourExperiences;
